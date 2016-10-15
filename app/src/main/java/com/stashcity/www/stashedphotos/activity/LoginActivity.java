380c3e9feace1f5fc4d29039b574e9deebb3c403
sravan kumar <sravankumar2828@gmai.com>
2016-10-15 16:59:48 +0530

    Initial commit

---
+++ app/src/main/java/com/stashcity/www/stashedphotos/activity/LoginActivity.java
@@ -0,0 +1,134 @@
+package com.stashcity.www.stashedphotos.activity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.google.gson.Gson;
+import com.stashcity.www.stashedphotos.ApplicationController;
+import com.stashcity.www.stashedphotos.R;
+import com.stashcity.www.stashedphotos.model.User;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    ProgressDialog progressDialog;
+    Button LoginButton;
+    String URL;
+    EditText Email;
+    EditText Password;
+    User userobj;
+
+    Gson gson;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //get the login url from string constants;
+        URL= getResources().getString(R.string.loginURL);
+        gson = new Gson();
+
+        LoginButton= (Button)findViewById(R.id.btn_login);
+        Email =(EditText)findViewById(R.id.input_email);
+        Password = (EditText)findViewById(R.id.input_password);
+        userobj=new User();
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                userobj.setEmail(Email.getText().toString().trim());
+                userobj.setPassword(Password.getText().toString().trim());
+                showProgressDialog();
+                login(userobj);
+
+
+            }
+        });
+
+    }
+
+    private void login(User user) {
+
+
+        HashMap<String, String> params = new HashMap<>();
+        params.put("Email", user.getEmail());
+        params.put("Password", user.getPassword());
+
+
+        JsonObjectRequest req = new JsonObjectRequest(URL, new JSONObject(params),
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response)
+                    {
+                        try
+                        {
+                            Log.e("CALL",response.toString());
+                            progressDialog.dismiss();
+                            userobj=gson.fromJson(response.toString(),User.class);
+                            if (userobj.getResponse().getStatus().equals("000"))
+                            {
+                                Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_LONG).show();
+
+                                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                startActivity(intent);
+                            }
+                            else
+                            {
+
+                                Toast.makeText(getApplicationContext(), "Login Failed", Toast.LENGTH_LONG).show();
+
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener()
+        {
+            @Override
+            public void onErrorResponse(VolleyError error)
+            {
+                VolleyLog.e("Error: ", error.getMessage());
+            }
+        });
+
+        // add the request object to the queue to be executed
+        ApplicationController.getInstance().addToRequestQueue(req);
+
+
+
+    }
+
+    public void showProgressDialog(){
+        progressDialog=new ProgressDialog(LoginActivity.this,
+                R.style.MyMaterialTheme);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Authenticating...");
+        Window window = progressDialog.getWindow();
+        window.setLayout(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT);
+        window.setGravity(Gravity.CENTER);
+        progressDialog.show();
+
+
+    }
+}
---
+++ app/src/main/java/com/stashcity/www/stashedphotos/activity/MainActivity.java
@@ -0,0 +1,205 @@
+package com.stashcity.www.stashedphotos.activity;
+
+import android.annotation.SuppressLint;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+
+import com.android.volley.Cache;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.stashcity.www.stashedphotos.ApplicationController;
+import com.stashcity.www.stashedphotos.R;
+import com.stashcity.www.stashedphotos.adapter.FeedListAdapter;
+import com.stashcity.www.stashedphotos.model.FeedItem;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements FragmentDrawer.FragmentDrawerListener{
+
+    private Toolbar mToolbar;
+    private FragmentDrawer drawerFragment;
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private ListView listView;
+    private FeedListAdapter listAdapter;
+    private List<FeedItem> feedItems;
+    private String URL_FEED = "http://services.stashcity.com/Service.svc/GetAllRecentImages";//getString(R.string.GetAllPostsURL);
+
+    @SuppressLint("NewApi")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        listView = (ListView) findViewById(R.id.list);
+        feedItems = new ArrayList<FeedItem>();
+        listAdapter = new FeedListAdapter(this, feedItems);
+        listView.setAdapter(listAdapter);
+
+
+        /* setting the tool bar*/
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        /* setting toolbar ends*/
+
+        drawerFragment = (FragmentDrawer)
+                getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), mToolbar);
+        drawerFragment.setDrawerListener(this);
+
+
+        Cache cache = ApplicationController.getInstance().getRequestQueue().getCache();
+        Cache.Entry entry = cache.get(URL_FEED);
+        if (entry != null) {
+            // fetch the data from cache
+            try {
+                String data = new String(entry.data, "UTF-8");
+                try {
+                    parseJsonFeed(new JSONObject(data));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+
+        }
+        else {
+            // making fresh volley request and getting json
+            JsonObjectRequest jsonReq;
+            jsonReq = new JsonObjectRequest(Request.Method.POST,
+                    URL_FEED, "", new Response.Listener<JSONObject>() {
+
+                @Override
+                public void onResponse(JSONObject response) {
+                    VolleyLog.d(TAG, "Response: " + response.toString());
+                    Log.e("responce", response.toString());
+                    if (response != null) {
+                        parseJsonFeed(response);
+                    }
+                }
+            }, new Response.ErrorListener() {
+
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    VolleyLog.d(TAG, "Error: " + error.getMessage());
+                }
+            });
+
+            // Adding request to volley request queue
+            ApplicationController.getInstance().addToRequestQueue(jsonReq);
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public void onDrawerItemSelected(View view, int position) {
+        displayView(position);
+    }
+
+    private void displayView(int position) {
+        Fragment fragment = null;
+        String title = getString(R.string.app_name);
+        switch (position) {
+            case 0:
+                fragment = new HomeFragment();
+                title = getString(R.string.title_home);
+                break;
+            case 1:
+                fragment = new FriendsFragment();
+                title = getString(R.string.title_friends);
+                break;
+            default:
+                break;
+        }
+
+        if (fragment != null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container_body, fragment);
+            fragmentTransaction.commit();
+
+            // set the toolbar title
+            getSupportActionBar().setTitle(title);
+        }
+    }
+
+    /**
+     * Parsing json reponse and passing the data to feed view list adapter
+     * */
+    private void parseJsonFeed(JSONObject response) {
+        try {
+            JSONArray feedArray = response.getJSONArray("GetAllRecentImagesResult");
+
+            for (int i = 0; i < feedArray.length(); i++) {
+                JSONObject feedObj = (JSONObject) feedArray.get(i);
+
+                FeedItem item = new FeedItem();
+                item.setId(feedObj.getInt("photograpId"));//id
+                item.setName(feedObj.getString("name"));//status
+
+                // Image might be null sometimes
+                String image = feedObj.isNull("url") ? null : feedObj
+                        .getString("url");//main image of post
+                item.setImge(image);
+                item.setStatus(feedObj.getString("name"));//status
+                item.setProfilePic(feedObj.getString("url"));//profilepic
+                item.setTimeStamp("1403375851930");//feedObj.getString("timeStamp"));//timestamp
+
+                // url might be null sometimes
+                String feedUrl = feedObj.isNull("url") ? null : feedObj
+                        .getString("url");
+                item.setUrl(feedUrl);
+
+                feedItems.add(item);
+            }
+
+            // notify data changes to list adapater
+            listAdapter.notifyDataSetChanged();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
